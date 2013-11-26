389ae90619f36eecbdce87d5ea3441126ad7558d
Lucas Cavalcante <lucasmcavalcante@gmail.com>
2013-11-26 20:52:38 -0300

    passo 4

--- lab2/conf/evolutions/default/1.sql
+++ lab2/conf/evolutions/default/1.sql
@@ -5,7 +5,7 @@
 
 create table task (
   id                        bigint not null,
-  label                     varchar(255),
+  nome                      varchar(255),
   constraint pk_task primary key (id))
 ;
 
